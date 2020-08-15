593ad13555d1e1bb3a64150682e961880c87d693
Jesús Bermúdez <jema0622@outlook.com>
2020-08-15 22:22:06 +0000

    Update keys.js

--- src/private/keys.js
+++ src/private/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
-    host: process.env.DATABASE_HOST || "localhost",
-    user: process.env.DATABASE_USER || "jema",
-    database: process.env.DATABASE_NAME || "cinema",
+    host: process.env.DATABASE_HOST || "host",
+    user: process.env.DATABASE_USER || "user",
+    database: process.env.DATABASE_NAME || "database",
     connectionLimit: 10,
-    password: process.env.DATABASE_PASSWORD || "2152"
+    password: process.env.DATABASE_PASSWORD || "password"
 }
