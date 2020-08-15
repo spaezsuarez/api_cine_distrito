d52a01c3a8c3cf90eb22f290be362e0a3c0db4cf
Jesús Bermúdez <jema0622@outlook.com>
2020-08-15 17:17:00 -0500

    Cambios sin console.log

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 const express = require('express');
-const app = express();
+const morgan = require('morgan');
+const cors = require('cors')
 const bodyParser = require('body-parser');
 const router = require('./routes/routes');
+const { request, response } = require('express');
+
+const app = express();
+
+app.use(morgan('dev'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(cors());
+
+app.use((request, response, next) => {
+    response.header("Access-Control-Allow-Origin", "https://cinemapa-frontend.herokuapp.com, localhost:5000");
+    response.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 router(app);
 
--- src/private/keys.js
+++ src/private/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
     host: process.env.DATABASE_HOST || "localhost",
-    user: process.env.DATABASE_USER || "user",
-    database: process.env.DATABASE_NAME || "database",
+    user: process.env.DATABASE_USER || "jema",
+    database: process.env.DATABASE_NAME || "cinema",
     connectionLimit: 10,
-    password: process.env.DATABASE_PASSWORD || "password"
+    password: process.env.DATABASE_PASSWORD || "2152"
 }
